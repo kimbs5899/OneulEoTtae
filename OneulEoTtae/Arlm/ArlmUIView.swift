8846b2eb7b981c92284b8cca4bfbb180cbade254
Matthew(매튜) <krsder@naver.com>
2024-01-16 19:46:00 +0900

    re2

---
+++ OneulEoTtae/Arlm/ArlmUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  OneulEoTtae
+//
+//  Created by Matthew on 1/16/24.
+//
+
+import SwiftUI
+
+struct AlrmUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
---
+++ OneulEoTtae/ArlmList/AlrmListUIView.swift
@@ -0,0 +1,18 @@
+//
+//  AlrmListUIView.swift
+//  OneulEoTtae
+//
+//  Created by Matthew on 1/16/24.
+//
+
+import SwiftUI
+
+struct AlrmListUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AlrmListUIView()
+}
