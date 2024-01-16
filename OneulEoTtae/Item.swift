dfb211381886b4b69cf0564b802f873c4a28cd5a
Matthew(매튜) <krsder@naver.com>
2024-01-16 19:43:01 +0900

    reSetting

---
+++ OneulEoTtae/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  OneulEoTtae
+//
+//  Created by Matthew on 1/16/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
